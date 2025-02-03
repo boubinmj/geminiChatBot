09a3c23d3093da4e20f216e50e85680c03d033dc
Matthew Boubin <mjb9353@nyu.edu>
2025-02-03 12:58:30 -0500

    changed listening port to 8080

--- app/server.py
+++ app/server.py
@@ -120,4 +120,4 @@ async def stream_chat_events(request: Input) -> StreamingResponse:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
